f8b2e47bc8ddbee1f0f6f36050013749768c4008
Thariga123 <tharigashrinithi@gmail.com>
2025-03-25 10:19:21 +0530

    Add files via upload

---
+++ Task 1/src/Hello.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hello = () => {
+  const myName = "Thariga!!"; 
+  return (
+    <div className="One">
+      <h1 className = "one">
+        Hello, {myName}
+      </h1>
+    </div>
+  );
+};
+
+export default Hello;
