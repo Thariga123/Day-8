ffd2db3660d0f26bae997990561735149d99868e
Thariga123 <tharigashrinithi@gmail.com>
2025-03-25 10:36:40 +0530

    Add files via upload

---
+++ Task 2,3/src/App.jsx
@@ -0,0 +1,11 @@
+import Studentname from './Studentname';
+
+function App() {
+  return (
+    <>
+  <Studentname name= "Student Name : R.Thariga Shrinithi"  class="Class : ECE"/>
+    </>
+)
+}
+
+export default App
---
+++ Task 2,3/src/Studentname.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Studentname(props){
+    return(
+    <div className="one">
+        <h4 className="One">{props.name}</h4>
+        <h4 className="One">{props.class}</h4>
+        
+    </div>
+    
+    )
+}
+
+export default Studentname
